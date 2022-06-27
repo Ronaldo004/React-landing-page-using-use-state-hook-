865dbd48b300a0e381c25f62dc5420b0e2595914
Muhammad rohan <rohanzahid11@gmail.com>
2022-06-27 05:44:24 -0700

    I commit

---
+++ src/Component/Basics/menuapi.js
@@ -0,0 +1,81 @@
+
+let Menu=[
+{
+    id:1,
+    catagory:"Lunch",
+    Name:"mggi",
+    price:100,
+    description:"Maggi (pronounced [ˈmaɡi] or pronounced [ˈmadːʒi]) is an international brand of seasonings, instant soups, and noodles that originated in Switzerland in the late 19th century. The Maggi company was acquired by Nestlé in 1947.",
+    image:"./img/noodles.jpg"
+
+},
+{
+    id:2,
+    catagory:"Lunch",
+    Name:"biryani",
+    price:230,
+    description:"We provide best spices and best quality bombay biryani any time at any place ",
+    image:"./img/biryani.jpg"
+
+},
+{
+    id:3,
+    Name:"Halwa puri",
+    catagory:"Break fast",
+    price:80,
+    description:"We provides you best quality halwa puri and chana salan thats you love its ",
+    image:"./img/halwapuri.jpg"
+
+},
+{
+    id:4,
+    Name:"Mutton-Karachi",
+    catagory:"Dinner",
+    price:1600,
+    description:"We provides you best quality mutton karahi at night any place.",
+    image:"./img/muttonkarahi.jpg"
+
+},
+{
+    id:5,
+    Name:"Chiken-karachi",
+    catagory:"Dinner",
+    price:1600,
+    description:" We provides you best quality chiken karahi at night any place.",
+    image:"./img/chikken.jpg"
+},
+{
+    id:6,
+    Name:"Pasta",
+    catagory:"Lunch",
+    price:250,
+    description:"We provides you best quality pasta at day time any place",
+    image:"./img/pasta.jpg"
+
+},
+{
+    id:7,
+    Name:"Cornflakes",
+    catagory:"Break fast",
+    price:130,
+    description:"We provides you best quality cornflakes at morning any place",
+    image:"./img/cornflakes.jpg"
+},
+{
+    id:8,
+    catagory:"Evening",
+    Name:"Platter Samosa Pakorey Kachori Allochap",
+    price:430,
+    description:"We provide best Evening Nashta with best quality ingredient any time at any place ",
+    image:"./img/Samosey plater.jpg"
+
+}
+]
+export default Menu;
+
+
+
+
+
+
+
